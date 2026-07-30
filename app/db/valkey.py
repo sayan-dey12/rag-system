c460d74d86c1667d3d5822b2ad1c0a78396dd8b7
SAYAN <sayandey1203@gmail.com>
2026-07-30 21:58:07 +0530

    change configuration in redis client

--- app/db/valkey.py
+++ app/db/valkey.py
@@ -5,5 +5,5 @@ from app.core.config import settings
 redis_client = redis.Redis(
     host=settings.VALKEY_HOST,
     port=settings.VALKEY_PORT,
-    decode_responses=True,
+    decode_responses=False,
 )
