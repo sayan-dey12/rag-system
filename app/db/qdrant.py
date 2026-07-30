8da8906c39e4b84a090816346c1169b962b29aba
SAYAN <sayandey1203@gmail.com>
2026-07-30 18:15:01 +0530

    creating valkey and qdrant client

---
+++ app/db/qdrant.py
@@ -0,0 +1,8 @@
+from qdrant_client import QdrantClient
+
+from app.core.config import settings
+
+qdrant_client = QdrantClient(
+    host=settings.QDRANT_HOST,
+    port=settings.QDRANT_PORT,
+)
---
+++ app/db/valkey.py
@@ -0,0 +1,9 @@
+import redis
+
+from app.core.config import settings
+
+redis_client = redis.Redis(
+    host=settings.VALKEY_HOST,
+    port=settings.VALKEY_PORT,
+    decode_responses=True,
+)
