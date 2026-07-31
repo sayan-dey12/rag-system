12ba688e6e334aa3194924766084afaefc0b018f
SAYAN <sayandey1203@gmail.com>
2026-07-31 08:14:29 +0530

    create chunker and changes in config file

--- app/services/chunking/chunker.py
+++ app/services/chunking/chunker.py
@@ -1,20 +1,16 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-
+from app.core.config import settings
 
 class DocumentChunker:
     """
     Splits LangChain Documents into smaller chunks.
     """
 
-    def __init__(
-        self,
-        chunk_size: int = 1000,
-        chunk_overlap: int = 200,
-    ):
+    def __init__(self,):
         self._splitter = RecursiveCharacterTextSplitter(
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
+            chunk_size=settings.CHUNK_SIZE,
+            chunk_overlap=settings.CHUNK_OVERLAP,
             length_function=len,
             add_start_index=True,
         )
